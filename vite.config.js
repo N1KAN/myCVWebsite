32c0a22eb6b0c105cfb3371867c73e5a44fb1f67
Nikan Seyed Marandy <nikan.seyedmarandy@stud.h-da.de>
2024-11-05 21:27:00 +0100

    eight commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    sourcemap: true,  // Für den Produktionsmodus
-  }
+  base: '/myCVWebsite/', // Dies sollte der Name des GitHub-Repositories sein
 })
